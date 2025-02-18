27d9b4f30cd48766796b00699e51651bfc1099df
Noman <noman.ict.iu@gmail.com>
2025-02-18 12:00:31 +0600

    Multidimentional Array

---
+++ multiDimentinal.js
@@ -0,0 +1,11 @@
+const arr = [
+  [78, 71, 80, 90],
+  [80, 75, 90, 86],
+  [71, 75, 79, 80],
+];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    console.log("element " + i + ":" + arr[i][j]);
+  }
+}
