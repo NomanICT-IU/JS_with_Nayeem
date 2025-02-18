c15acc3887aacf01336f6d3a637ef74c4be41cb7
Noman <noman.ict.iu@gmail.com>
2025-02-18 11:29:11 +0600

    Array splice method

---
+++ arraySplice.js
@@ -0,0 +1,12 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// main array
+console.log( arr)
+
+//adding some value after removing some value
+arr.splice(3, 2,11,12);
+console.log( arr);
+
+//removing value only
+
+arr.splice(3, 2);
+console.log( arr);
